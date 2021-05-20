4cd01c8d23428629f22ab8e0d78ce32fc6a3614c
shobhit35c <79322844+shobhit35c@users.noreply.github.com>
2021-05-20 09:10:43 -0400

    trying to add something

---
+++ Planet.py
@@ -0,0 +1,35 @@
+class Planet():
+
+
+    def __init__(self):
+        print("address of self =", id(self))
+
+
+    def Mercury(self):
+
+
+        print("Planet Mercury")
+
+        import _json
+
+    def Venus(self):
+
+
+    def Earth(self):
+
+        print("Earth planet facts")
+
+    def Mars(self):
+
+        print("Mars facts")
+
+
+
+
+obj = Planet
+
+
+
+
+
+
