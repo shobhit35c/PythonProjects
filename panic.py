53e42ad5b68c5da27a8dfaf35a49e8bdf2450ee8
shobhit35c <79322844+shobhit35c@users.noreply.github.com>
2021-06-13 13:05:45 -0400

    working with Lists

---
+++ panic.py
@@ -0,0 +1,13 @@
+phrase = "Don't panic!"
+
+plist = list(phrase)
+
+print(phrase)
+print(plist)
+
+new_phrase = ''.join([plist[1], plist[2],plist[5],plist[4],plist[7],plist[6]])
+
+print(plist)
+print(new_phrase)
+
+
